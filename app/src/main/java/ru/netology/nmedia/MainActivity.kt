0e49dfc713bfa6e3a95cf252b8dc8c8938d2ae9a
Nima <ekkond89@mail.ru>
2025-08-16 21:39:55 +0300

    Обработка событий2

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -75,10 +75,18 @@ class MainActivity : AppCompatActivity() {
     private fun formatCount(count: Int): String {
         return when {
             count < 1000 -> count.toString()
-            count < 10_000 -> String.format("%.1fK", count / 1000.0).replace(",", ".")
-            count < 1_000_000 -> "${count / 1000}K"
-            else -> String.format("%.1fM", count / 1_000_000.0).replace(",", ".")
+            count < 10_000 -> {
+                 String.format("%.1fK", count / 1000.0).replace(",", ".")
+            }
+            count < 1_000_000 -> {
+                "${count / 1000}K"
+            }
+            else -> {
+                String.format("%.1fM", count / 1_000_000.0).replace(",", ".")
+            }
         }
     }
+
 }
+
 
